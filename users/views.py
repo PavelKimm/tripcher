6b1224aeac703fa98c27a5b8b053a3114b9abfd8
Pavel Kim <fastredialer@gmail.com>
2019-12-05 03:17:14 +0700

    users app was created

---
+++ users/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from .forms import UserForm, ProfileForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
+
+
+@login_required
+@transaction.atomic
+def update_profile(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, _('Your profile was successfully updated!'))
+            return redirect('settings:profile')
+        else:
+            messages.error(request, _('Please correct the error below.'))
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, '', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
