c452f6dee2887000c585b7a32aff9c53afc59b14
Pavel Kim <fastredialer@gmail.com>
2019-12-09 21:45:05 +0700

    minor fix

--- users/serializers.py
+++ users/serializers.py
@@ -52,14 +52,14 @@ class SocialSerializer(serializers.Serializer):
     provider = serializers.CharField(max_length=255, required=True)
     access_token = serializers.CharField(max_length=4096, required=True, trim_whitespace=True)
 
-    def create(self, validated_data):
-        """
-        Create and return a new `User` instance, given the validated data.
-        """
-        return User.objects.create(**validated_data)
+    # def create(self, validated_data):
+    #     """
+    #     Create and return a new `User` instance, given the validated data.
+    #     """
+    #     return User.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        instance.username = validated_data.get('user', instance.username)
-        instance.email = validated_data.get('email', instance.email)
-        instance.save()
-        return instance
+    # def update(self, instance, validated_data):
+    #     instance.username = validated_data.get('user', instance.username)
+    #     instance.email = validated_data.get('email', instance.email)
+    #     instance.save()
+    #     return instance
