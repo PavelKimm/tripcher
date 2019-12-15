dd01998dacdf58959a223c05b4a51772a05487f9
Pavel Kim <fastredialer@gmail.com>
2019-12-15 13:21:56 +0700

    day, week and month filtering are implemented

--- blog/urls.py
+++ blog/urls.py
@@ -10,16 +10,19 @@ router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     path('posts/', PostList.as_view(), name='posts-list'),
+
     path('posts/create/', PostCreation.as_view(), name='post-creation'),
     path('posts/create/save_as_draft/', DraftCreation.as_view(), name='draft-creation'),
+
+    path('posts/<int:pk>/', PostDetail.as_view(), name='post-detail'),
+    path('posts/<int:pk>/like/', like, name='post-like'),
 
     path('drafts/', DraftList.as_view(), name='drafts-list'),
     path('drafts/<int:pk>/', DraftDetail.as_view(), name='draft-detail'),
     path('drafts/<int:pk>/confirm/', PostCreation.as_view(), name='draft-confirmation'),
 
-    path('posts/<int:pk>/', PostDetail.as_view(), name='post-detail'),
-    path('posts/<int:pk>/like/', like, name='post-like'),
-
     path('', include(router.urls)),
     path('posts/<int:pk>/comments/create/', CommentViewSet.as_view({'post': 'create'}), name='comment-add'),
+
+    path('posts/<str:period>/', PostList.as_view(), name='posts-list'),
 ]
